02050b919d993bc4d6f6f5f6474f6dc34f0674ab
mariana <marianaxrp@gmail.com>
2021-05-04 13:43:40 -0300

    created api and service to fetch posts from it

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import "./assets/css/base/base.css";
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
 import Pagina404 from './paginas/Pagina404';
+import Cabecalho from './componentes/Cabecalho';
+
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <Cabecalho />
       <Switch>
         <Route exact path='/'>
           <Home />
---
+++ src/componentes/Cabecalho.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import image from '../assets/img/doguito.svg';
+import '../assets/css/componentes/cabecalho.css';
+import { Link } from 'react-router-dom';
+
+const Cabecalho = () => {
+    return (
+        <header className="cabecalho container">
+            <div className="menu-hamburger">
+                <span className="menu-hamburger__icone">
+                </span>
+            </div>
+            <div className="cabecalho-container">
+                <Link to="/" className="flex flex--centro">
+                    <img className="cabecalho__logo" src={image} alt="logo Doguito" />
+                    <h1 className="cabecalho__titulo">Petshop</h1>
+                </Link>
+            </div>
+            <nav className="menu-cabecalho">
+                <ul className="menu-itens">
+                    <li><Link to="#" className="menu-item menu-item--entrar">Entrar</Link></li>
+                    <li><Link to="#" className="menu-item">Produtos</Link></li>
+                    <li><Link to="/" className="menu-item">Blog</Link></li>
+                    <li><Link to="/sobre" className="menu-item">Sobre</Link></li>
+                </ul>
+                <div className="menu-cabecalho-background">
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Cabecalho;
